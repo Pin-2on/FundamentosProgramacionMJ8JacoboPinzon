601dbbc37c5178b2a1439ded4861cb871266dc1e
Pin-2on <jacobo.pinzon@upb.edu.co>
2026-07-30 08:18:19 -0500

    Primer commit

---
+++ FundamentosProgramacionMJ8JacoboPinzon/README.md
@@ -0,0 +1,2 @@
+# FundamentosProgramacionMJ8JacoboPinzon
+Son los ejercicios realizados en la clase de fundamentos de programación
---
+++ FundamentosProgramacionMJ8JacoboPinzon/VariablesConstantes/VariablesConstantes/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace VariablesConstantes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Aqui va el código fuente
+            //Variables
+            string nombre = "Jacobo";
+            Console.WriteLine("Bienvenido" + nombre + "al curso de Fundamentos de Programación");
+        }
+    }
+}
