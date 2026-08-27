238a1e6ef268565bd518a22b2b9edc967a704664
Pin-2on <jacobo.pinzon@upb.edu.co>
2026-08-27 09:30:01 -0500

    Ejercicios Ciclos While 8 y 9

---
+++ 8.CicloWhile.1/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Realizar un algoritmo que pida números enteros positivos y los sume, hasta que se ingrese un número entero negativo. Se debe mostrar una patalla con el total de la suma de los números ingresados
+
+            int SumaEnterosPositivos = 0; //Acumulador
+            int numero = 0;
+
+            Console.WriteLine("Ingrese un número para sumar, Ingrese -1 para terminar");
+            numero = int.Parse(Console.ReadLine());
+
+            while (numero >= 0)
+            {
+                SumaEnterosPositivos += numero;
+                Console.WriteLine("Ingrese un número para sumar, Ingrese -1 para terminar");
+                numero = int.Parse(Console.ReadLine());
+
+            }
+
+            Console.WriteLine($"La suma de los números positivos ingresados es: {SumaEnterosPositivos}");
+
+        }
+    }
+}
---
+++ 9.CicloWhile.2/9.CicloWhile.2/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.IO;
+
+namespace _9.CicloWhile._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // //Realizar un algoritmo que le pida al usuario ingresar por teclado números enteros al azar. Al usuario le corresponde ingresar la cantidad de números que va a introducir. El algoritmo debe escribir:
+            // //La catidad de números introducidos mayores a 0. La cantidad de números introducidos menores que 0. la cantidad de números iguales a 0.
+
+            // int CantidadNum = 0;
+            // int NumIngresado = 0;
+            // int NumContador = 1;
+
+            // int NumMayor0 = 0;
+            // int NumIgual0 = 0;
+            // int NumMenor0 = 0;
+
+
+            // Console.WriteLine("Ingrese la cantidad de números que desea ingresas (ud vera cuanto se queda aca");
+            // CantidadNum = int.Parse(Console.ReadLine());
+
+            // if (CantidadNum < 0)
+            // {
+
+            //     Console.WriteLine("Tan chistoso, Ingrese uno positivo");
+
+            // }
+
+
+            // Console.WriteLine("Ingrese el número que usted desee (De cualquier signo)");
+
+            // NumIngresado = int.Parse(Console.ReadLine());
+
+            // while (NumIngresado > 0 && CantidadNum != NumContador)
+            // {
+
+            //         NumMayor0 += 1;
+            //         Console.WriteLine("Ingrese el número que usted desee (De cualquier signo)");
+            //         NumIngresado = int.Parse(Console.ReadLine());
+
+            //         NumContador += 1;
+
+            // }
+
+            //while (NumIngresado == 0 && CantidadNum != NumContador)
+            //{
+
+            //         NumIgual0 += 1;
+            //         Console.WriteLine("Ingrese el número que usted desee (De cualquier signo)");
+            //         NumIngresado = int.Parse(Console.ReadLine());
+
+            //         NumContador += 1;
+
+            //}
+
+            //while (NumIngresado < 0 && CantidadNum != NumContador)
+            //{
+
+            //         NumMenor0 += 1;
+            //         Console.WriteLine("Ingrese el número que usted desee (De cualquier signo)");
+            //         NumIngresado = int.Parse(Console.ReadLine());
+
+            //         NumContador += 1;
+
+            //}
+
+
+            //     Console.WriteLine($"Numeros mayores a 0: {NumMayor0}");
+            //     Console.WriteLine($"Numeros iguales a 0: {NumIgual0}");
+            //     Console.WriteLine($"Numeros menor a 0: {NumMenor0}");
+
+
+            int cantidadNumeros = 0;
+            int numero = 0;
+            int contador = 0;
+            int contadorPositivos = 0;
+            int contadorNegativos = 0;
+            int contadorCeros = 0;
+
+            Console.WriteLine("Ingrese la cantidad de números a evaluar");
+
+            cantidadNumeros = int.Parse(Console.ReadLine());
+
+            while (cantidadNumeros > contador)
+            {
+
+                contador++;
+                Console.WriteLine("ingrese el número a evaluar");
+                numero = int.Parse(Console.ReadLine());
+
+
+                if(numero < 0)
+                {
+
+                    contadorNegativos++;
+                }
+                else
+                {
+                    if(numero == 0)
+                    {
+                        contadorCeros++;
+                    }
+                    else
+                    {
+
+                        contadorPositivos++;
+
+                    }
+
+                }
+
+            }
+
+
+            Console.WriteLine($"Cantidad Positivos: {contadorPositivos}. Cantidad Negativos: {contadorNegativos}. Cantidad Ceros: {contadorCeros}");
+
+        }
+    }
+}
