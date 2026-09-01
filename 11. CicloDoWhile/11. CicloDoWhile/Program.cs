bfc40067af0c3bc9ae0a978a4b8926ecdae4d1e6
Pin-2on <jacobo.pinzon@upb.edu.co>
2026-09-01 09:27:43 -0500

    11.Ciclo do while

---
+++ 11. CicloDoWhile/11. CicloDoWhile/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.IO;
+
+
+namespace _11.CicloDoWhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ////CicloDo While
+            //int contador = 1;
+            //int acumulador 0;
+
+            //do
+            //{
+            //    acumulador += contador;
+            //    contador++;
+            //} while (contador <= 5);
+
+            //Console.WriteLine($"La suma de los cinco primeros números enteros es: {acumulador}");
+
+            //int numero = 0;
+            //int contador = 1;
+            //int acumulador = 0;
+
+            //string No;
+
+
+
+            //Console.WriteLine("Porvafor Ingrese un número entero");
+
+            //numero = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine($"Tabla de multiplicar de {numero}");
+
+            //do
+            //{
+            //    acumulador++;
+            //    Console.WriteLine($"{contador}. {numero * acumulador}");
+            //    contador++;
+
+            //} while (contador <= 10);
+
+            //Console.WriteLine("¿Quiere generar otra tabla (Si o no)?");
+            //No = Convert.ToString(Console.ReadLine());
+
+            //while (No == "Si" || No == "si")
+            //{
+            //    numero = 0;
+            //    contador = 1;
+            //    acumulador = 0;
+
+            //    No = "";
+
+            //    Console.WriteLine("Porvafor Ingrese un número entero");
+
+            //    numero = int.Parse(Console.ReadLine());
+
+            //    Console.WriteLine($"Tabla de multiplicar de {numero}");
+
+            //    do
+            //    {
+            //        acumulador++;
+            //        Console.WriteLine($"{contador}. {numero * acumulador}");
+            //        contador++;
+
+            //    } while (contador <= 10);
+
+            //    Console.WriteLine("¿Quiere generar otra tabla (Si o no)?");
+            //    No = Convert.ToString(Console.ReadLine());
+            //    }
+
+            string no;
+            int contador = 1;
+            int numero = 0;
+            int divisor = 1;
+            divisor = 1;
+
+
+            Console.WriteLine("¿Calcular Primos? lol (No/si)");
+
+            no = Convert.ToString(Console.ReadLine());
+
+            if (no == "si" || no == "SI" || no == "Si")
+            {
+
+                do
+                {
+                    numero++;
+
+
+                    contador++;
+
+                    Console.WriteLine($"{numero}");
+
+                } while (contador <= 100);
+
+
+
+            }
+        }
+    }
+}
